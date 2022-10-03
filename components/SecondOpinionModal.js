9b229885cbcfdac55e54b0a3813d5d76c02418c5
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-10-03 17:37:32 -0500

    1)finished breakpoint adjustments for SecondOpinion modal...2)Close button works...NEXT:: Rig of both the email and push to next project

--- components/SecondOpinionModal.js
+++ components/SecondOpinionModal.js
@@ -1,34 +1,45 @@
 import {useEffect,useState} from "react";
 import {createPortal} from "react-dom";
+import { useRouter } from 'next/router'
 
-const SecondOpinionModal = ({isOpen})=>{
+const SecondOpinionModal = ({isOpen,setIsOpen})=>{
 
 	return isOpen
-		? createPortal(<Content/>,
+		? createPortal(<Content setIsOpen={setIsOpen}/>,
 		  document.querySelector("#secondopinion-portal"))
 		: null
 }
 
-const Content = ()=>{
+const Content = ({setIsOpen})=>{
+
+	const router = useRouter();
+	function closeModal(){
+		setIsOpen(false)
+	};
+	function closeModalNextProject(){
+		setIsOpen(false);
+		router.push('/')
+	};
+
 	return (<>
-		<div className="absolute flex justify-center items-center bg-blue-700 opacity-70 top-24 inset-12 backdrop-blur-xl">
-			<table className="w-full h-full">
-				<tr className="">
-					<td className="h-4 p-5 text-center b">
-						<h1 className="text-3xl so-font-main font-bold ">Thank you for asking..</h1>
-					</td>
-					<td className="flex justify-center items-center h-full p-5">
-						<div className="text-xl so-font-main font-bold">X</div>
-					</td>	
-				</tr>
-				<tr className="b">
-					<td>
-						<div className="text-5xl so-font-main">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eum placeat iure veritatis, illo quaerat error in debitis saepe a porro facilis officiis perferendis ipsa.
-						</div>
-					</td>
-				</tr>
-			</table>
+		<div className="absolute flex justify-center items-center opacity-1 top-8 inset-12 lg:top-8 ">
+
+			<div id="secondOpinionModal-grid" className="w-full  grid grid-cols-1 grid-rows-['175px_auto'] so-border md:w-3/4 lg:w-4/5 lg:h-3/5">
+				<div className="flex justify-between items-center px-5 bg-blue-400 py-5">
+					<h1 className="text-3xl so-font-main font-bold ">Thank you for asking..</h1>
+					<div id="secondOpinionModal-closeButton" className="text-xl so-font-main font-bold" onClick={closeModal}>X</div>
+				</div>
+				<div className="flex flex-col justify-center items-center so-border bg-gradient-to-t from-[#070709ff] to-blue-900 py-5">
+					<div className="text-4xl so-font-main text-right px-10 pb-10">
+						...so, this was just a demo landing page 
+						for a made-up company, but if you got this far,
+						you'll receive a demo confirmation email. 
+						<br/>
+						<br/><div className="w-full text-center text-blue-400">And again, thanks for clicking through.</div>
+					</div>
+					<button className="border-2 border-blue-400 p-4 rounded-full bg-blue-400/20 hover:bg-blue-400">Close and go to the next project</button>
+				</div>
+			</div>
 		</div>
 		
 	</>)
--- pages/landing-pages/second-opinion-landing-page.js
+++ pages/landing-pages/second-opinion-landing-page.js
@@ -13,10 +13,10 @@ export default function SecondOpinion(){
 	}
 
 return (<>
-<SecondOpinionModal isOpen={isOpen}/>
+<SecondOpinionModal isOpen={isOpen} setIsOpen={setIsOpen}/>
 
 <div className="container flex justify-center items-center  my-0 mx-auto border-box lg:py-20 lg:bg-transparent  lg:mx-auto ">
-	<div className="grid grid-rows-[225px_120px_1fr_1fr]  so-border lg:bg-[#070709ff] lg:grid-cols-[348px_400px_340px] lg:grid-rows-4  lg:h-[644px] ">
+	<div className="grid grid-rows-[225px_120px_1fr_1fr] bg-[#070709ff] so-border lg:bg-[#070709ff] lg:grid-cols-[348px_400px_340px] lg:grid-rows-4  lg:h-[644px] ">
 		<div id="logo-row" className="grid grid-col-1 grid-rows-[99.9%_.1%] border-r-2 overflow-hidden lg:grid-rows-[.1%_99.9%] lg:row-span-4 " >
 			<Image src="/images/the_second_opinion_grp-logo(simple).png" className="row-start-1 row-span-1 object-contain scale-90 lg:hidden lg:opacity-0" width={175} height={115} alt={"a screenshot"}/>
 			<Image src="/images/second-opinion-lunar.png" className="row-start-2 row-span-1 opacity-0 lg:row-start-1 lg:row-span-1 lg:object-contain lg:inline lg:opacity-100" width={348} height={644} alt="lunar curvature" />
@@ -32,15 +32,15 @@ return (<>
 			<form action="" className="w-1/2 text-xl pr-5 lg:w-full lg:pr-8 ">
 				<div id="form-topSection" className="lg:relative lg:-top-12">
 					<div className="py-2 grid grid-cols-[auto_192px] md:grid-cols-[auto_220px] lg:grid-cols-[auto_192px]  ">
-						<div id="url-label" className="so-font-main font-bold text-right pr-4 md:pr-2">http:// </div>
+						<div id="url-label" className="so-font-main font-bold text-right pr-4 md:pr-2" required>http:// </div>
 						<input id="url-input" className="so-font-main text-base text-[#8fbbff] text-left pl-2 bg-slate-700 placeholder:text-[#62799c] rounded-full md:w-full" type="url" name="url" placeholder="www.yourSite.com" />
 					</div>
 					<div className="py-2 grid grid-cols-[auto_192px] md:grid-cols-[auto_220px] lg:grid-cols-[auto_192px] ">
-						<div id="email-label" className="so-font-main font-bold text-right pr-4 md:pr-2" >email:</div>
+						<div id="email-label" className="so-font-main font-bold text-right pr-4 md:pr-2" required>email:</div>
 						<input id="email-input" className="so-font-main text-base text-[#8fbbff] text-center bg-slate-700 placeholder:text-[#62799c] rounded-full md:w-full" type="email"  name="email" placeholder="youremail@email.com" />
 					</div>
 					<div className="py-2 grid grid-cols-[auto_192px] md:grid-cols-[auto_220px] lg:grid-cols-[auto_192px] ">
-						<div id="contact-label" className="so-font-main font-bold text-right pr-4 md:pr-2" >contact:</div>
+						<div id="contact-label" className="so-font-main font-bold text-right pr-4 md:pr-2" required>contact:</div>
 						<input id="contact-input" className="so-font-main text-base text-[#8fbbff] text-center bg-slate-700 placeholder:text-[#62799c] rounded-full md:w-full" type="text"  name="contact" placeholder="Who will be talking to?" />
 					</div>
 				</div>
