43cb7e02eed08932e01395264df855a502828ab4
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-11-30 11:04:01 -0600

    update

--- pages/api/send-grid-mail.js
+++ pages/api/send-grid-mail.js
@@ -5,20 +5,24 @@ sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 export default async function handler(req, res) {
   
   const email = req.body
-  // console.log('body', email);
+
   const msg = {
       to: email,
-      from: 'GSdaBlessedFist@gmail.com',
-      subject: 'New Message!',
-      html: `<div>${email}</div>`
+      from: {
+        email: 'GSdaBlessedFist@gmail.com',
+        name: "Jason Zamora"
+      },
+      templateId: "d-2de9ee6b657c4f1bba2338cc28bd67e1",
+      dynamic_template_data:{
+        recipientEmail : email
+      }
     }
+
   try{
     sendgrid.send(msg).then(()=>{
       res.status(200).json({status: `Email sent`});
     })
   }catch (error) {
     console.error(error);
-    // expected output: ReferenceError: nonExistentFunction is not defined
-    // Note - error messages will vary depending on browser
   }
 }
